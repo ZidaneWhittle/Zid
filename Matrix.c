5b81011ea9000bbbd04d1df3fac650597f397afb
ZidaneWhittle <36480144+ZidaneWhittle@users.noreply.github.com>
2018-03-16 10:37:29 -0500

    Add files via upload

---
+++ Matrix.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define SIZE 3
+
+
+int main()
+{
+
+int a[][],b[][],c[][];
+
+int i,j,num,num2;
+for(i=0;i<3;i++)
+{
+    scanf("%d",&num);
+    for(j=0;j<3;j++)
+{
+    scanf("%d",&num2);
+}
+}
+    for(i=0;i<SIZE;i++)
+    {
+
+
+        for(j=0;j<SIZE;j++)
+        {
+         c[i][j]=a[i][j]-b[i][j];
+        }
+
+
+    }
+}
